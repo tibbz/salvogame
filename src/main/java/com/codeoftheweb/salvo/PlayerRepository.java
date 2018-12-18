7252dbd22e832ca079192f35307cc32254819d05
tibbz <sarahgaco@gmail.com>
2018-12-18 11:50:44 +0100

    salvogame

---
+++ src/main/java/com/codeoftheweb/salvo/PlayerRepository.java
@@ -0,0 +1,13 @@
+package com.codeoftheweb.salvo;
+
+import java.util.List;
+
+import com.codeoftheweb.salvo.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+    List<Player> findByUserName(String userName);
+}
+
---
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -0,0 +1,29 @@
+package com.codeoftheweb.salvo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SalvoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SalvoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(PlayerRepository repository) {
+		return (args) -> {
+			// save a couple of customers
+			repository.save(new Player("Jack"));
+			repository.save(new Player("Chloe"));
+			repository.save(new Player("Kim"));
+			repository.save(new Player("David"));
+			repository.save(new Player("Michelle"));
+		};
+
+	}
+}
+
