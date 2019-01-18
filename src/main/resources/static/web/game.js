6fa87d09924812bfbd7fd7f0ac44635cde28bc23
tibbz <sarahgaco@gmail.com>
2019-01-18 13:25:24 +0100

    unfinished salvo

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -3,12 +3,20 @@
 function createtable() {
 
 var conts = document.getElementById("stable");
+var tblbody = document.createElement("tbody");
 
 for (var x = 0; x < 10; x++){
-var row = document.createElement("th");
 
-row.appendChild("stable");
+var row = document.createElement("tr");
+var column = document.createElement("td");
+
+
+row.appendChild("column");
+tblbody.appendChild("row");
+conts.appendChild("tblbody");
 
 }
 
 }
+
+createtable ();
